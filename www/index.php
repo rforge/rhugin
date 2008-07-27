d4beadcdb49f521547e6c100aad847959237640d
kjell <kjell@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-07-27 12:06:05 +0000

    Added example.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rhugin@14 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -33,7 +33,7 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
 
 <h1>The RHugin Package Homepage</h1>
 
-The RHugin package provides an R API for the Hugin Decision Engine. Here is a link to the project <a href="http://r-forge.r-project.org/projects/rhugin/">summary</a> page.
+The RHugin package provides an API for the Hugin Decision Engine. Here is a link to the project <a href="http://r-forge.r-project.org/projects/rhugin/">summary</a> page.
 
 <h2>Installation</h2>
 
@@ -48,6 +48,121 @@ RHugin requires a functional installation of Hugin Expert. If you have a license
 <p>
 *The RHugin package automatically sets the <code>HUGINHOME</code> environment variable on Microsoft Windows systems if Hugin is installed in <code>C://Program Files//Hugin Expert//Hugin Researcher 6.9</code>.
 </p>
+
+<h2>Quick Introduction</h2>
+
+Once the RHugin package is installed, load it into an R session using the <code>library</code> function.
+
+<pre>
+     library(RHugin)
+</pre>
+
+You can list all of the functions available in the package using the <code>ls</code> function.
+
+<pre>
+     ls("package:RHugin")
+      [1] "add.edge"                   "add.node"                  
+      [3] "clone.node"                 "compile"                   
+      [5] "condense.data.frame"        "delete.edge"               
+      [7] "delete.node"                "error.code"                
+      [9] "error.description"          "expand.grid.sans.coercion" 
+     [11] "generate.nodes"             "generate.tables"           
+     [13] "get.belief"                 "get.cases"                 
+     [15] "get.edges"                  "get.finding"               
+     [17] "get.nodes"                  "get.normalization.constant"
+     [19] "get.states"                 "get.table"                 
+     [21] "get.table.nodes"            "hugin.domain"              
+     [23] "initialize.domain"          "print.RHuginDomain"        
+     [25] "print.summary.RHuginDomain" "propagate"                 
+     [27] "read.hkb"                   "reset"                     
+     [29] "retract"                    "reverse.edge"              
+     [31] "RHugin.check.args"          "RHugin.handle.error"       
+     [33] "set.cases"                  "set.finding"               
+     [35] "set.states"                 "set.table"                 
+     [37] "simulate.RHuginDomain"      "summary.RHuginDomain"      
+     [39] "switch.parent"              "write.hkb"                 
+</pre>
+
+Documentation for each of these functions is provided using the R help system. For example, use the command
+
+<pre>
+     help(add.node)
+</pre>
+
+to see the documentation for the <code>add.node</code> function.
+
+
+<h3>An Example</h3>
+
+Lets use the RHugin package to build the bayesian network considered in this Hugin <a href="http://www.hugin.com/developer/tutorials/BN_example/">tutorial</a>.
+
+First we need to create a Hugin domain:
+
+<pre>
+     AppleTree <- hugin.domain()
+</pre>
+
+Then we add the nodes:
+
+<pre>
+     add.node(AppleTree, "Sick", states = c("yes", "no"))
+     add.node(AppleTree, "Dry", states = c("yes", "no"))
+     add.node(AppleTree, "Loses", states = c("yes", "no"))
+</pre>
+
+Next add the edges:
+
+<pre>
+     add.edge(AppleTree, "Loses", "Sick")
+     add.edge(AppleTree, "Loses", "Dry")
+</pre>
+
+After adding all of the nodes and edges, we specify the conditional probabilities. The best practice for setting a conditional probability table (CPT) is to use the <code>get.table</code> function to get the CPT (as a data frame) for a given node from the Hugin domain, set the conditional probabilities, then use the <code>set.table</code> function to actually set the CPT for the given node in the Hugin domain.
+
+<pre>
+     sick.table <- get.table(AppleTree, "Sick")
+     sick.table[, 2] <- c(0.1, 0.9)
+     set.table(AppleTree, "Sick", sick.table)
+</pre>
+
+Repeat for Dry:
+
+<pre>
+     dry.table <- get.table(AppleTree, "Dry")
+     dry.table[, 2] <- c(0.1, 0.9)
+     set.table(AppleTree, "Dry", dry.table)
+</pre>
+
+And for Loses:
+
+<pre>
+     loses.table <- get.table(AppleTree, "Loses")
+     loses.table[, 4] <- c(0.95, 0.05, 0.85, 0.15, 0.90, 0.1, 0.02, 0.98)
+     set.table(AppleTree, "Loses", loses.table)
+</pre>
+
+Then compile the domain:
+
+<pre>
+     compile(AppleTree)
+</pre>
+
+Enter some evidence:
+
+<pre>
+     set.finding(AppleTree, "Loses", "yes")
+</pre>
+
+And finally, propagate and get beliefs:
+
+<pre>
+     propagate(AppleTree)
+     get.belief(AppleTree, "Sick")
+           yes        no 
+     0.4939956 0.5060044 
+</pre>
+
+If we don't make any mistakes we get the same answer as the tutorial.
 
 </body>
 </html>
