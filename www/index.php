403353f9d0f2ea7a6a6b3f35fdfaf35a6d1b7eb5
kjell <kjell@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-10-28 16:46:49 +0000

    Webpage update.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rhugin@75 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -33,16 +33,14 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
 
 <h1>The RHugin Package Homepage</h1>
 
-The RHugin package provides an R API for the Hugin Decision Engine. The RHugin package is free software and is distributed under the terms of the LGPL.  The Hugin Decision Engine is commercial software and can be purchased from <a href="http://www.hugin.com">www.hugin.com</a>.  Initially, the intended audience for this package is Hugin users who would like to integrate the statistical and programatic capabilities of R into their analyses.
+The RHugin package provides an R API for the Hugin Decision Engine. The RHugin package is free software and is distributed under the terms of the LGPL.  The Hugin Decision Engine is commercial software which is available at <a href="http://www.hugin.com">www.hugin.com</a>.  Presently, the target audience for this package is Hugin users who would like to integrate the statistical and programatic capabilities of R into their analyses.
 <br><br>
-The RHugin package is compatible with the evaluation version of Hugin: Hugin Lite.
-<br><br>
-The project's source code and development statistics are available on the <a href="http://r-forge.r-project.org/projects/rhugin/">summary</a> page.
+Code and development statistics are available on the <a href="http://r-forge.r-project.org/projects/rhugin/">summary</a> page.
 
 <h2>Installation</h2>
 
 <p>
-RHugin requires a functional installation of Hugin Expert. If you have a licensed Hugin installation then simply install the RHugin package and load it into your R session. Please note that the <code>HUGINHOME</code> environment variable must be set as described in the Hugin documentation. If you don't already have Hugin installed then you can download an evaluation version from <a href="http://www.hugin.com">www.hugin.com</a>. The evaluation version is limited but sufficient to explore the capabilities of the RHugin package.
+RHugin has an external dependency on Hugin Expert. If you have a Hugin license then simply install the RHugin package and load it into your R session. Please note that the <code>HUGINHOME</code> environment variable must be set as described in the Hugin documentation (this is done automatically on Windows). If you do not have a Hugin license it is still possible to try the package: RHugin is compatible with the evaluation version of Hugin (again see <a href="http://www.hugin.com">www.hugin.com</a>). The evaluation version is limited but sufficient to explore the capabilities of the RHugin package.
 </p>
 
 <h4>Non-Windows</h4>
@@ -62,145 +60,43 @@ Building the package on Windows is quite difficult so binary packages are provid
 </p>
 
 <ul>
-  <li> <a href="binary/Windows/RHugin_0.3-1.zip">RHugin_0.3-1.zip</a> (2008-08-22)
-  <li> <a href="binary/Windows/RHugin_0.4-2.zip">RHugin_0.4-2.zip</a> (2008-09-01)
-  <li> <a href="binary/Windows/RHugin_0.4-3.zip">RHugin_0.4-3.zip</a> (2008-09-05)
   <li> <a href="binary/Windows/RHugin_0.4-5.zip">RHugin_0.4-5.zip</a> (2008-09-28)
   <li> <a href="binary/Windows/RHugin_0.4-6.zip">RHugin_0.4-6.zip</a> (requires R 2.7.2 and Hugin Researcher 7.0 : 2008-10-15)
   <li> <a href="binary/Windows/RHugin_0.4-10.zip">RHugin_0.4-10.zip</a> (requires R 2.7.2 and Hugin Researcher/Lite 7.0 : 2008-10-21)
 </ul>
 
 <p>
-Download the most recent version and install it using the "Install package(s) from local zip files..." item from the R Packages menu.
+Download an appropriate binary and install it using the "Install package(s) from local zip files..." item from the R Packages menu.
 </p>
 
 <h4>Rgraphviz</h4>
 
 <p>
-If the Rgraphviz package is available then RHugin will be able to automatically draw Hugin domains as well as position nodes in hkb and net files. If possible install this package too. If Rgraphviz is not available the rest of the RHugin package will function normally.
+RHugin can take advantage of the Rgraphviz package to draw Hugin domains in R and to position nodes in hkb and net files.
 </p>
 
 <p>
-The Rgraphviz package is available from <a href="http://www.bioconductor.org">bioconductor</a> and has an external dependency on <a href="http://www.graphviz.org">graphviz</a>.  A binary release of graphviz for Windows can be downloaded here: <a href="http://www.graphviz.org/pub/graphviz/stable/windows/graphviz-2.20.2.exe">graphviz-2.20.2.exe</a>.  The Windows binary for Rgraphviz provided by bioconductor is currently broken so I am temporarily hosting a working Windows binary: <a href="binary/Windows/Rgraphviz_1.18.1.zip">Rgraphviz_1.18.1.zip</a> (built for R 2.7.2 and graphviz 2.20.2).
+Non-Windows users can follow the Bioconductor <a href="http://bioconductor.org/packages/2.3/bioc/html/Rgraphviz.html">instructions</a> for installing Rgraphviz.
 </p>
 
+<p>
+Windows users can install Rgraphviz by following these three easy steps:
+
+<ol>
+  <li> Download and install Graphviz version 2.20.3.<br><ul><li><a href="http://www.graphviz.org/pub/graphviz/stable/windows/graphviz-2.20.3.msi">http://www.graphviz.org/pub/graphviz/stable/windows/graphviz-2.20.3.msi</a></ul><br>
+  <li> Add <code>C:\Program Files\Graphviz 2.21\bin</code> to your Path (surprisingly this is actually where the installer puts 2.20.3 - you may have to adjust this setting on non-English systems or if Graphviz is installed anywhere other than the default location).<br><br>
+  <li> Download and install (using the 'Install package(s) from local zip files...' item in  the R Packages menu) the <a href="binary/Windows/Rgraphviz_1.20-0.zip">Rgraphviz</a> package.  Please use the Rgraphviz binary provided here - the official Bioconductor Windows binary is usually broken.  Please note that this binary requires R 2.8.0 and Graphviz 2.20.3.
+</ol>
 
 <br>
 
-<h2>Quick Introduction</h2>
+The Rgraphviz package must be loaded manually - <code>library(Rgraphviz)</code> - before using the RHugin functions <code>plot</code> and <code>position.nodes<code>.
 
-Once the RHugin package is installed, load it into an R session using the <code>library</code> function.
+<br>
+<br>
+<br>
+<br>
 
-<pre>
-     library(RHugin)
-</pre>
-
-You can list all of the functions available in the package using the <code>ls</code> function.
-
-<pre>
-     ls("package:RHugin")
-      [1] "add.edge"                   "add.node"                  
-      [3] "clone.node"                 "compile"                   
-      [5] "condense.data.frame"        "delete.edge"               
-      [7] "delete.node"                "error.code"                
-      [9] "error.description"          "expand.grid.sans.coercion" 
-     [11] "generate.nodes"             "generate.tables"           
-     [13] "get.belief"                 "get.cases"                 
-     [15] "get.edges"                  "get.finding"               
-     [17] "get.nodes"                  "get.normalization.constant"
-     [19] "get.states"                 "get.table"                 
-     [21] "get.table.nodes"            "hugin.domain"              
-     [23] "initialize.domain"          "print.RHuginDomain"        
-     [25] "print.summary.RHuginDomain" "propagate"                 
-     [27] "read.hkb"                   "reset"                     
-     [29] "retract"                    "reverse.edge"              
-     [31] "RHugin.check.args"          "RHugin.handle.error"       
-     [33] "set.cases"                  "set.finding"               
-     [35] "set.states"                 "set.table"                 
-     [37] "simulate.RHuginDomain"      "summary.RHuginDomain"      
-     [39] "switch.parent"              "write.hkb"                 
-</pre>
-
-Documentation for each of these functions is provided using the R help system. For example, use the command
-
-<pre>
-     help(add.node)
-</pre>
-
-to see the documentation for the <code>add.node</code> function.
-
-
-<h3>An Example</h3>
-
-Lets use the RHugin package to build the bayesian network considered in this Hugin <a href="http://www.hugin.com/developer/tutorials/BN_example/">tutorial</a>.
-
-First we need to create a Hugin domain:
-
-<pre>
-     AppleTree <- hugin.domain()
-</pre>
-
-Then we add the nodes:
-
-<pre>
-     add.node(AppleTree, "Sick", states = c("yes", "no"))
-     add.node(AppleTree, "Dry", states = c("yes", "no"))
-     add.node(AppleTree, "Loses", states = c("yes", "no"))
-</pre>
-
-Next add the edges:
-
-<pre>
-     add.edge(AppleTree, "Loses", "Sick")
-     add.edge(AppleTree, "Loses", "Dry")
-</pre>
-
-After adding all of the nodes and edges, we specify the conditional probabilities. The best practice for setting a conditional probability table (CPT) is to use the <code>get.table</code> function to get the CPT (as a data frame) for a given node from the Hugin domain, set the conditional probabilities, then use the <code>set.table</code> function to actually set the CPT for the given node in the Hugin domain.
-
-<pre>
-     sick.table <- get.table(AppleTree, "Sick")
-     sick.table[, 2] <- c(0.1, 0.9)
-     set.table(AppleTree, "Sick", sick.table)
-</pre>
-
-Repeat for Dry:
-
-<pre>
-     dry.table <- get.table(AppleTree, "Dry")
-     dry.table[, 2] <- c(0.1, 0.9)
-     set.table(AppleTree, "Dry", dry.table)
-</pre>
-
-And for Loses:
-
-<pre>
-     loses.table <- get.table(AppleTree, "Loses")
-     loses.table[, 4] <- c(0.95, 0.05, 0.85, 0.15, 0.90, 0.1, 0.02, 0.98)
-     set.table(AppleTree, "Loses", loses.table)
-</pre>
-
-Then compile the domain:
-
-<pre>
-     compile(AppleTree)
-</pre>
-
-Enter some evidence:
-
-<pre>
-     set.finding(AppleTree, "Loses", "yes")
-</pre>
-
-And finally, propagate and get beliefs:
-
-<pre>
-     propagate(AppleTree)
-     get.belief(AppleTree, "Sick")
-           yes        no 
-     0.4939956 0.5060044 
-</pre>
-
-If we don't make any mistakes we get the same answer as the tutorial.
 
 </body>
 </html>
