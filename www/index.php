2a033102f084ce0773d37dc00a1523f95777436c
kjell <kjell@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-02-05 12:26:43 +0000

    Updated webssite for 0.7-12 release.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rhugin@185 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -33,7 +33,7 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
 
 <h1>The RHugin Package Homepage</h1>
 
-The RHugin package provides a platform for building and making inference from Bayesian belief networks by integrating the Hugin Decision Engine into the R environment for statistical computing.  The Hugin Decision Engine is commercial software and is required in order to use the RHugin package.  Information about Hugin is available from the Hugin website: <a href="http://www.hugin.com">www.hugin.com</a>.  Since a Hugin license is required, the target audience for this package is Hugin users who would like to integrate the statistical and programatic capabilities of R into their Hugin workflow.  For those interested in evaluating Hugin and/or the RHugin package, a trial version of Hugin called Hugin-Lite is available (also from the Hugin website).
+The RHugin package provides a platform for building and making inference from Bayesian belief networks by integrating the Hugin Decision Engine into the R environment for statistical computing.  The Hugin Decision Engine is commercial software and is required in order to use the RHugin package.  Information about Hugin is available from the Hugin website: <a href="http://www.hugin.com">www.hugin.com</a>.  Since a Hugin license is required, the target audience for this package is Hugin users who would like to integrate the statistical and programatic capabilities of R into their Hugin workflow.  For those interested in evaluating Hugin, a trial version of Hugin called Hugin Lite is available.
 
 <br><br>
 
@@ -46,6 +46,7 @@ Code and development statistics for the RHugin project are available on the <a h
 <h2>News</h2>
 
 <ul>
+<li>2010-02-05: RHugin 0.7-12 released.
 <li>2009-07-01: RHugin 0.7-2 adds support for continuous nodes and influence diagrams. This release requires Hugin 7.1 and R 2.9.1.
 <li>2009-04-30: RHugin 0.6-6 is compatible with Hugin 7.1 and R 2.9.0.</li>
 <li>2009-02-11: RHugin 0.6 is now available.  The plotting of RHugin domains has been improved.</li>
@@ -55,18 +56,14 @@ Code and development statistics for the RHugin project are available on the <a h
 <h2>Installation</h2>
 
 <p>
-If Hugin is already installed on your computer then simply follow the directions below to install the RHugin package.  If Hugin is installed in a non-standard location then you must have the <code>HUGINHOME</code> environment variable set in order to use the package (see the <a href="http://www.hugin.com/developer/documentation/API_Manuals">Hugin C API Manual</a>).  Also, you will need to modify the <code>HUGINHOME</code> variable in the installation instructions as appropriate.
-</p>
-
-<p>
-If you do not have a licensed copy of Hugin and would like to evaluate the RHugin package, you will first need to obtain and install the trial version of Hugin: Hugin-Lite.
+If your Hugin installation is in a non-standard location, you must set the <code>HUGINHOME</code> environment variable in order to use the package (see the <a href="http://www.hugin.com/developer/documentation/API_Manuals">Hugin C API Manual</a>).  Also, you will need to modify the <code>HUGINHOME</code> variable in the installation instructions as appropriate.
 </p>
 
 
 <h4>Dependencies</h4>
 
 <p>
-The RHugin packages depends on the <code>graph</code> and <code>gRain</code> packages.  These two packages (and their dependencies) are available on CRAN.  Run the command
+The RHugin package depends on the <code>graph</code> and <code>gRain</code> packages.  These two packages (and their dependencies) are available on CRAN.  Run the command
 </p>
 
 <pre>
@@ -92,17 +89,17 @@ Linux users can install the package using the <code>install.packages</code> func
 <h4>Installing the RHugin Package on Mac OS X</h4>
 
 <p>
-A binary package is provided for R on Mac OS X.
+Mac OS X users can install the package using the <code>install.packages</code> function as well. The <code>HUGINHOME</code> environment variable must be set to the full path of the Hugin Decision Engine folder. In most cases typing <code>Sys.setenv(HUGINHOME = "/Applications/HDE</code> then pressing <code>tab</code> will give the correct path.  The following commands install the package for Hugin Lite.
 </p>
 
-<ul>
-<li> <a href="binary/MacOSX/RHugin_0.7-5.tgz">RHugin_0.7-5.tgz</a> (built for R 2.10.0 and Hugin Researcher/Lite 7.1: 2009-11-24)</li>
-</ul>
+<pre>
+    Sys.setenv(HUGINHOME = "/Applications/HDE7.2-lite")
+    install.packages("RHugin", repos = "http://R-Forge.R-project.org", type = "source")
+</pre>
 
 <p>
-Install <code>RHugin_0.7-5.tgz</code> using the <i>Package Installer</i> menu item from the R <i>Packages & Data</i> menu.
+Note that, as of version 0.7-12, the RHugin package dynamically links to the Hugin Decision Engine hence Hugin 7.2 or later is required.
 </p>
-
 
 <h4>Installing the RHugin Package on Microsoft Windows</h4>
 
@@ -111,7 +108,7 @@ A binary package is provided for Windows.
 </p>
 
 <ul>
-<li> <a href="binary/Windows/RHugin_0.7-5.zip">RHugin_0.7-5.zip</a> (built for R 2.10.0 and Hugin Researcher/Lite 7.1: 2009-11-24)</li>
+<li> <a href="binary/Windows/RHugin_0.7-12.zip">RHugin_0.7-12.zip</a> (built for R 2.10.1 and Hugin Researcher/Lite 7.2: 2010-02-05)</li>
 </ul>
 
 <p>
